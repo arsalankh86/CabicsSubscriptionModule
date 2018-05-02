c8048c53df3e989e4990aea5e16a3e78c6dddf2a
arsalandev <arsalankhan9286@gmail.com>
2018-05-03 02:30:43 +0500

    working on admin

---
+++ CabicsSubscription.Web/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CabicsSubscription.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddPlan()
+        {
+            return View();
+        }
+
+
+        public ActionResult ViewPlan()
+        {
+            return View();
+        }
+
+
+
+    }
+}
